8785d7f043529a94e868ddfb91abd7f457dc5ee5
Igor3550 <igormeloigormelo@gmail.com>
2022-08-26 16:28:06 -0300

    função: POST /sign-up

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ import express from "express";
 import cors from 'cors';
 
 const app = express()
-app.use(cors)
+app.use(cors())
 app.use(express.json())
+
+/*Estrutura
+{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}
+{
+	username: "bobesponja",
+  tweet: "eu amo o hub"
+}
+*/
+
+const arrayUsers = []
+const arrayTweets = []
+
+app.post('/sign-up', (req, res) => {
+  const user = req.body
+  arrayUsers.push(user);
+  console.log(arrayUsers)
+  res.send('OK');
+})
 
 app.listen(5000, () => console.log('Listen on port 5000...'))
