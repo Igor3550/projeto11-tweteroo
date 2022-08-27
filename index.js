e37e9af4b3b792cb3768fe4d8384ef639a297244
Igor3550 <igormeloigormelo@gmail.com>
2022-08-27 18:23:24 -0300

    funçao: GET /tweets

--- index.js
+++ index.js
@@ -16,20 +16,55 @@ app.use(express.json())
 }
 */
 
+function userIsInList (user, list) {
+  for (let u of list){
+    if(u.username === user){
+      return true
+    }
+  }
+  return false;
+}
+
 const arrayUsers = []
 const arrayTweets = []
 
 app.post('/sign-up', (req, res) => {
   const user = req.body
   arrayUsers.push(user);
+  console.log(user)
   res.send('OK');
 })
 
 app.post('/tweets', (req, res) => {
   const tweet = req.body
-  arrayTweets.push(tweet)
-  console.log(arrayTweets)
-  res.send('OK');
+  if(userIsInList(tweet.username, arrayUsers)){
+    arrayTweets.push(tweet)
+    res.send('OK');
+  }else{
+    res.sendStatus(401);
+  }
+})
+
+app.get('/tweets', (req, res) => {
+  
+  let tweetsList = []
+  if(arrayTweets.length <= 10){
+    tweetsList = arrayTweets
+  }else{
+    tweetsList = arrayTweets.slice(arrayTweets.length-10);
+  }
+
+  const newTweetsList = tweetsList.map((tweet) => {
+    let avatar = ''
+    arrayUsers.forEach(user => {
+      if(user.username === tweet.username) avatar = user.avatar
+    })
+    return {...tweet, avatar: avatar}
+  })
+  console.log(tweetsList);
+  console.log(arrayTweets);
+
+  res.send(newTweetsList)
 })
 
 app.listen(5000, () => console.log('Listen on port 5000...'))
